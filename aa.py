068cba2e8622879700e3e55a5c388468eba6171f
Alinx <30387309+GZ-Alinx@users.noreply.github.com>
2019-08-14 17:10:17 +0800

    Update aa.py

--- aa.py
+++ aa.py
@@ -3,7 +3,8 @@ import pymysql
 
 #def query(name):
 # 连接数据库
-db = pymysql.connect("14.18.117.150", "monitor", "Sheer6Theiroophe@", "vps_monitor")
+db = pymysql.connect("数据库地址", "用户名", "密码", "数据库")
+")
 cur = db.cursor()
 sql = """SELECT AVG(band)AS avgBand,alias FROM vps_monitor WHERE alias="node1" AND update_time > DATE_SUB(NOW(),INTERVAL 1 HOUR)"""
 cur.execute(sql)
